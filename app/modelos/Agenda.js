8a04ce4beb00d0b325db2c90b0d17250138b0985
aguz11 <agus110196@gmail.com>
2024-11-07 13:18:18 -0300

    Update Agenda.js

--- app/modelos/Agenda.js
+++ app/modelos/Agenda.js
@@ -1,7 +1,8 @@
 // models/Agenda.js
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/database'); // Asegúrate de que este archivo exporte la instancia de Sequelize
 
+
+module.exports = (sequelize, DataTypes) => {
 class Agenda extends Model {}
 
 // Definición del modelo
@@ -36,56 +37,23 @@ Agenda.init({
     },
   },
 }, {
-  sequelize, // La instancia de Sequelize
+  sequelize, 
   modelName: 'Agenda',
   tableName: 'agendas',
-  timestamps: false, // Si no quieres que Sequelize agregue campos de timestamps (createdAt, updatedAt)
+  timestamps: false, 
 });
 
-module.exports = (sequelize, DataTypes) => {
-  const Agenda = sequelize.define('Agenda', {
-      id_agenda: {
-          type: DataTypes.INTEGER,
-          autoIncrement: true,
-          primaryKey: true,
-      },
-      clasificacion: {
-        type: DataTypes.STRING(50), 
-        allowNull: true, 
-      },
-      estado: {
-        type: DataTypes.STRING(50),
-          allowNull: true,
-      },
-      disponible_desde: {
-        type: DataTypes.DATE, 
-        allowNull: false, 
-      },
-      disponible_hasta: {
-        type: DataTypes.DATE, 
-        allowNull: false, 
-      },
-      medico_id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          references: {
-              model: 'medicos', // Nombre de la tabla de referencia
-              key: 'id_medico', // Llave primaria en la tabla de referencia
-          },
-      },
-  }, {
-      tableName: 'agendas',
-      timestamps: false, // Si no quieres que Sequelize agregue campos de timestamps
-  });
+
+
 
   // Relación con el modelo Medico
   Agenda.associate = (models) => {
       Agenda.belongsTo(models.Medico, {
-          foreignKey: 'medicoId',
+          foreignKey: 'medico_id',
           targetKey: 'id_medico',
       });
       Agenda.hasMany(models.Turno, {
-          foreignKey: 'agendaId',
+          foreignKey: 'agenda_id',
           sourceKey: 'id_agenda',
       });
   };
