3655490ebf5e7d788b2174fc8e1299eb795b0021
aguz11 <agus110196@gmail.com>
2024-11-07 13:21:56 -0300

    Update Turno.js

--- app/modelos/Turno.js
+++ app/modelos/Turno.js
@@ -1,7 +1,8 @@
 // models/Turno.js
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/database'); // Asegúrate de que este archivo exporte la instancia de Sequelize
 
+
+module.exports = (sequelize, DataTypes) => {
 class Turno extends Model {}
 
 // Definición del modelo
@@ -55,89 +56,31 @@ Turno.init({
     },
   },
 }, {
-  sequelize, // La instancia de Sequelize
+  sequelize, 
   modelName: 'Turno',
   tableName: 'turnos',
-  timestamps: false, // Si no quieres que Sequelize agregue campos de timestamps (createdAt, updatedAt)
+  timestamps: false, 
 });
 
-module.exports = (sequelize, DataTypes) => {
-  const Turno = sequelize.define('Turno', {
-    id_turno: { 
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    fecha_hora: { 
-      type: DataTypes.DATE, 
-      allowNull: false,
-    },
-    estado: {
-      type: DataTypes.STRING(50),
-        allowNull: false,
-    motivo_consulta: { 
-      type: DataTypes.TEXT,
-      allowNull: true, 
-    },
-    creado_en: { 
-      type: DataTypes.DATE, 
-      allowNull: false, 
-      defaultValue: DataTypes.NOW, 
-    },
-    medico_id: { 
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'medicos', 
-        key: 'id_medico', 
-      },
-    },
-    paciente_id: { 
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'pacientes', 
-        key: 'id_paciente', 
-      },
-    },
-    especialidad_id: { 
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'especialidades', 
-        key: 'id_especialidad', 
-      },
-    },
-    agenda_id: { 
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'agendas', 
-        key: 'id_agenda', 
-      },
-    },
-  },  
-  },  {
-    tableName: 'turnos', 
-    timestamps: false, 
-  });
+
+
 
 // Relación con los modelos
 Turno.associate = (models) => {
   Turno.belongsTo(models.Paciente, {
-    foreignKey: 'pacienteId',
+    foreignKey: 'paciente_id',
     targetKey: 'id_paciente',
   });
   Turno.belongsTo(models.Medico, {
-    foreignKey: 'medicoId',
+    foreignKey: 'medico_id',
     targetKey: 'id_medico',
   });
   Turno.belongsTo(models.Especialidad, {
-    foreignKey: 'especialidadId',
+    foreignKey: 'especialidad_id',
     targetKey: 'id_especialidad',
   });
   Turno.belongsTo(models.Agenda, {
-    foreignKey: 'agendaId',
+    foreignKey: 'agenda_id',
     targetKey: 'id_agenda',
   });
 };
