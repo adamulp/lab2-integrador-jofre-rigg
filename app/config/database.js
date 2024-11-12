0004dcb90d8548472842e8f8583b0ff69cf93a3c
aguz11 <agus110196@gmail.com>
2024-11-12 15:13:52 -0300

    Update database.js

--- app/config/database.js
+++ app/config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: process.env.DB_CLIENT || 'mysql', 
 });
